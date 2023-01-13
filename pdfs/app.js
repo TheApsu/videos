d1e571be8177b67ae379eb5e6d538d2a825df929
Jose Mena <sistemas88@red5g.co>
2023-01-13 11:22:06 -0500

    NUEVOS PDF

--- pdfs/app.js
+++ pdfs/app.js
@@ -1,6 +1,6 @@
 // If absolute URL from the remote server is provided, configure the CORS
 // header on that server.
-var url = 'JESUS.pdf';
+var url = 'JESÚS ES EL CAMINO.pdf';
 
 // Loaded via <script> tag, create shortcut to access PDF.js exports.
 var pdfjsLib = window['pdfjs-dist/build/pdf'];
